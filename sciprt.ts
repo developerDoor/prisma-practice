7e8ed4f68490d360f947ccb6ccf640fe94062c09
jhmoon <dothebestmoon1016@gmail.com>
2023-04-04 11:23:26 +0900

    nested reads :: include

--- sciprt.ts
+++ sciprt.ts
@@ -1,9 +1,27 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+// 만약 DB가 최대 5개의 동시 연결을 지원하는 경우 이 PrismaClient는 이러한 5개의 연결을 모두 처리한다.
+// 이 PrismaClient의 인스턴스는 하나만 사용하고 여러개 만들지 않는 것이 정말 중요하다.
+// 그렇지 않을 경우 너무 많은 연결로 인해 DB가 정체될 수 있다.
 
 async function main() {
-    // ... you will write your Prisma Client queries here
-    const user = await prisma.user.create({ data: { name: "Moon" } })
+    await prisma.user.deleteMany()
+    const user = await prisma.user.create({
+        data: {
+            name: "moon",
+            email: "moon@test.com",
+            age: 29,
+            userPreference: {
+                create: {
+                    emailUpdates: true,
+                },
+            },
+        },
+        include: {
+            userPreference: true,
+        }
+    })
+
     console.log(user)
 }
 
